9d9566c7e368d3e6dd18561a83371400efca879a
mazenswar <mazenswar@protonmail.com>
2023-06-18 13:56:10 -0400

    initial commit v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Better Learn To React Fool
         </a>
       </header>
     </div>
